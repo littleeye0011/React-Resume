f565b075bcd81d016acf82e25cec0842756dbe61
littleeye0011 <eiemily2014@gmail.com>
2022-01-27 16:08:30 +0700

    fix commit

--- react-resume/src/components/page/Product.jsx
+++ react-resume/src/components/page/Product.jsx
@@ -1,10 +1,10 @@
-import { Link } from "react-router-dom";
-
 const Product = () => {
   return (
     <div>
       <h1>เนื้อหา Product</h1>
-      <Link to="https://8c90v.csb.app/">Project</Link>
+      <a href="https://8c90v.csb.app/" rel="noreferrer" target="_blank">
+        test link
+      </a>
     </div>
   );
 };
